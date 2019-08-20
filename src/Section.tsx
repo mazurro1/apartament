50c8ff514418a222774508a3e986bcbea130b411
mazurro1 <mazul96.hm@gmail.com>
2019-08-20 19:16:29 +0200

    start project

---
+++ src/Section.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export interface SectionProps {}
+
+export default class Section extends React.Component<SectionProps, {}> {
+  render() {
+    return <h1>Hello from and !</h1>;
+  }
+}
