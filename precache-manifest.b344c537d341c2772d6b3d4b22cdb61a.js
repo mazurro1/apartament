087c99965eb6e028433572ffa3552b844dfbb3b6
dominik.pasternak <dominik.pasternak@nexio.pl>
2019-10-29 14:12:06 +0100

    Updates

--- precache-manifest.b344c537d341c2772d6b3d4b22cdb61a.js
+++ precache-manifest.b344c537d341c2772d6b3d4b22cdb61a.js
@@ -4,44 +4,44 @@ self.__precacheManifest = [
     "url": "/apartament/static/media/nerwus.5a033bc6.otf"
   },
   {
-    "revision": "859d8927810d08e2fc5a",
-    "url": "/apartament/static/css/main.c3520b6d.chunk.css"
+    "revision": "788e5928fa4bb49640bc",
+    "url": "/apartament/static/css/main.62acedab.chunk.css"
   },
   {
-    "revision": "c6d64f33ae15b0582816",
-    "url": "/apartament/static/js/runtime~main.1d11de6d.js"
+    "revision": "3e97dfb0706866d03eae",
+    "url": "/apartament/static/js/runtime~main.66678620.js"
   },
   {
     "revision": "8c91eeb9e689c52e1a9a02ff56842460",
     "url": "/apartament/static/media/bgimage.8c91eeb9.jpg"
   },
   {
-    "revision": "e95dc70ac2e811138dea",
-    "url": "/apartament/static/js/2.bdb0b535.chunk.js"
+    "revision": "4b52cda8be92091ec54e",
+    "url": "/apartament/static/js/2.a2199096.chunk.js"
   },
   {
     "revision": "fbaab6bbc9b63eeda3d03fc5acb2232d",
     "url": "/apartament/static/media/video.fbaab6bb.mp4"
   },
   {
-    "revision": "4896a5e29c406e7558bf",
-    "url": "/apartament/static/js/3.9d8addd3.chunk.js"
+    "revision": "09db5f851680eefd2b6d",
+    "url": "/apartament/static/js/3.b4960e62.chunk.js"
   },
   {
-    "revision": "14ec4f7c6f94a9a9acd3",
-    "url": "/apartament/static/js/4.78883c93.chunk.js"
+    "revision": "23fe91d7ff586978ce38",
+    "url": "/apartament/static/js/4.50b998eb.chunk.js"
   },
   {
-    "revision": "f78a5c0b70fb2cbf8f38",
-    "url": "/apartament/static/js/5.ad8f7c6c.chunk.js"
+    "revision": "10e5179f9bdd31e8c3f4",
+    "url": "/apartament/static/js/5.a3e95e59.chunk.js"
   },
   {
-    "revision": "2767ff1d130d33ad5ff3",
-    "url": "/apartament/static/js/6.00aed5cb.chunk.js"
+    "revision": "cf6901bdc671a9c18a86",
+    "url": "/apartament/static/js/6.ff537161.chunk.js"
   },
   {
-    "revision": "e40d6b110d2e2490094d",
-    "url": "/apartament/static/js/7.d719b8e0.chunk.js"
+    "revision": "2928870e2f74898e2f2f",
+    "url": "/apartament/static/js/7.fad13e5f.chunk.js"
   },
   {
     "revision": "ad9626a4df4a3f89f9086520c5f0f79d",
@@ -52,16 +52,16 @@ self.__precacheManifest = [
     "url": "/apartament/static/media/nerwus.36da487f.woff"
   },
   {
-    "revision": "162a801f011040cb28849384ad7ebc72",
-    "url": "/apartament/static/media/nerwus.162a801f.eot"
-  },
-  {
     "revision": "3c28d955655f7c241c6ad241f4e99293",
     "url": "/apartament/static/media/nerwus.3c28d955.ttf"
   },
   {
-    "revision": "859d8927810d08e2fc5a",
-    "url": "/apartament/static/js/main.c9114ed4.chunk.js"
+    "revision": "162a801f011040cb28849384ad7ebc72",
+    "url": "/apartament/static/media/nerwus.162a801f.eot"
+  },
+  {
+    "revision": "788e5928fa4bb49640bc",
+    "url": "/apartament/static/js/main.8bf02b13.chunk.js"
   },
   {
     "revision": "167435b40b14fd2baa69b1fc9bab215d",
@@ -112,15 +112,15 @@ self.__precacheManifest = [
     "url": "/apartament/static/media/11.76f28d89.JPG"
   },
   {
-    "revision": "4896a5e29c406e7558bf",
+    "revision": "09db5f851680eefd2b6d",
     "url": "/apartament/static/css/3.329251b7.chunk.css"
   },
   {
-    "revision": "e95dc70ac2e811138dea",
+    "revision": "4b52cda8be92091ec54e",
     "url": "/apartament/static/css/2.bcb5a4a4.chunk.css"
   },
   {
-    "revision": "0c4500cab3ab3e9e6f5b7b83d6ac96f0",
+    "revision": "3a1c9ed12e46ae93d77bc7ec81080a4e",
     "url": "/apartament/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/apartament/precache-manifest.0578e9e4b8d4798c97db9767d2a1367e.js"
+  "/apartament/precache-manifest.b344c537d341c2772d6b3d4b22cdb61a.js"
 );
 
 workbox.clientsClaim();
